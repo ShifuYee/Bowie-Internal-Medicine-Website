4a5941d18de331cb940a8b2a3b5756d60d3d06db
Shifu Yee <20980584+ShifuYee@users.noreply.github.com>
2018-06-28 13:55:27 -0400

    feat: add tabs to the description area

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 // React + Gatsby
 import React from 'react'
-import Link from 'gatsby-link'
-
-// Components
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+// When using webpack, the stylesheet must be imported
+import 'react-tabs/style/react-tabs.css';
 
 // Images
 import homeimage from '../images/home-page-image.jpg'
@@ -14,7 +14,7 @@ const HomePage = () => (
     </div>
     <img alt="Home Page Image" src={homeimage} />
     <div className="home-body">
-      <h1>Bowie Internal Medicine Associates</h1>
+      <h1>Welcome to Bowie Internal Medicine</h1>
       <p>
         Bowie Internal Medicine is a group of four physicians 
         and two certified nurse practitioners specializing in 
@@ -23,6 +23,57 @@ const HomePage = () => (
         all general medical and many minor surgical and 
         gynecological problems.
       </p>
+      <Tabs>
+        <TabList>
+          <Tab>Primary Care</Tab>
+          <Tab>Gastroenterology</Tab>
+          <Tab>Office Hours</Tab>
+          <Tab>Patient Portal</Tab>
+        </TabList>
+        <TabPanel>
+          <h2>TODO 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>TODO 2</h2>
+        </TabPanel>
+        <TabPanel>
+          <table className="office-hours">
+            <tr>
+              <td>Monday</td>
+              <td>8:30 AM – 6:30 PM</td>
+            </tr>
+            <tr>
+              <td>Tuesday</td>
+              <td>8:30 AM – 5:00 PM</td>
+            </tr>
+            <tr>
+              <td>Wednesday</td>
+              <td>8:30 AM – 7:00 PM</td>
+            </tr>
+            <tr>
+              <td>Thursday</td>
+              <td>8:30 AM – 6:30 PM</td>
+            </tr>
+            <tr>
+              <td>Friday</td>
+              <td>8:30 AM – 2:45 PM</td>
+            </tr>
+            <tr>
+              <td colSpan="2">Closed on weekends</td>
+            </tr>
+          </table>
+        </TabPanel>
+        <TabPanel>
+          <div itemprop="description">
+            <p>
+              This is a service, which is being offered as a convenience. 
+              If you require a more immediate response to your prescription 
+              needs, please contact our office to make an appointment.
+            </p>
+            <a href="#">Visit Portal</a>
+          </div>
+        </TabPanel>
+      </Tabs>
     </div>
   </div>
 );
