afae991c5427c727c44fa9a84f6c411dc0bb95cd
Shifu Yee <20980584+ShifuYee@users.noreply.github.com>
2018-07-04 19:53:24 -0400

    update: added a service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,19 @@ module.exports = {
   siteMetadata: {
     title: `Bowie-Internal-Medicine-Website`,
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet', 
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Bowie-Internal-Medicine-Website",
+        short_name: "BowieIntMed",
+        start_url: "/",
+        display: "minimal-ui",
+        icon: "src/images/favicon.png"
+      }
+    },
+    'gatsby-plugin-offline'    
+  ],
   pathPrefix: `/Bowie-Internal-Medicine-Website`
 }
