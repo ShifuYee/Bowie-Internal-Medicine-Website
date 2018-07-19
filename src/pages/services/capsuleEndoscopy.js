264a63ea1c28a72d8f53b5a4c215b4b5a097017f
Shifu Yee <20980584+ShifuYee@users.noreply.github.com>
2018-07-18 22:45:56 -0400

    feat: added content to all services pages

--- src/pages/services/capsuleEndoscopy.js
+++ src/pages/services/capsuleEndoscopy.js
@@ -1,11 +1,107 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import homeimage from '../../images/home-page-image.jpg'
+
 const CapsuleEndoscopy = () => (
-  <div className="container">
-    <h1>Capsule Endoscopy</h1>
-    <br />
-    <Link to="/">Go back to the homepage</Link>
+  <div className="service-content">
+    <img alt="Home Page Image" src={homeimage} className="img-responsive" />
+    <h1 className="main-subject">Capsule Endoscopy (Camera Pill)</h1>
+    <p>
+      For the first time ever, images of the entire small intestine 
+      are obtainable to assist the physician in the diagnosis and 
+      treatment of gastrointestinal diseases.
+      <br />
+      <br />
+      Capsule Endoscopy is a "state of the art" equipment. 
+      Gastrointestinal diseases affect millions of people in the 
+      United States annually. Diseases of the Small intestine are 
+      difficult to diagnose due to the limitations of current 
+      diagnostic tools. These limitations often lead to multiple 
+      procedures without definite diagnosis. Capsule Endoscopy is 
+      an innovative diagnostic tool that is non-invasive, easy to 
+      perform and provides a substantially improved level of visual 
+      imaging for early detection and diagnosis of gastrointestinal 
+      tract diseases.
+    </p>
+    <h2 className="sub-topic">What is Capsule Endoscopy ?</h2>
+    <p>
+      Capsule Endoscopy is a non-invasive, high quality, disposable, 
+      diagnostic imaging device for the use in the gastrointestinal 
+      tract. Capsule Endoscopy uses a disposable miniature video 
+      camera contained in a capsule (Camera Pill Endoscope) that 
+      is ingested by the patient. It is propelled by peristalsis 
+      to naturally move through the digestive tract. The Camera 
+      Pill contains a complete optical system with a video chip, 
+      light source, transmitter, and energy source designed 
+      specifically for direct imaging within the intestinal tract. 
+      The camera takes two images per second, which is approximately 
+      50,000 pictures in a 8 hour period. It transmits the data to 
+      a recording device worn around the waist of the patient.
+      <br />
+      <br /> 
+      Once the study is completed, the recording device is connected 
+      to a computer whose software provides the images to the computer 
+      screen. The procedure is ambulatory, allowing patients to 
+      continue daily activities throughout the endoscopic examination.
+    </p>
+    <h2 className="sub-topic">What preparation is required ?</h2>
+    <p>
+      Basically, for capsule endoscopy, the patient has to fast for 
+      at least ten hours prior to the procedure. The day before the 
+      procedure it is essential that liquid diet be maintained starting 
+      at 8:00p.m. and nothing to eat or drink after midnight.
+    </p>
+    <h2 className="sub-topic">What about my current medications ?</h2>
+    <p>
+      Most medications may be continued as usual, but some can interfere 
+      with the preparation or the examination. Inform your physician 
+      of your current medications several days prior to your visit. 
+      Aspirin products, arthritis medicines, anticoagulants (blood 
+      thinner such as Coumadin), insulin and iron products are examples. 
+      It is advised not to take any medications before the procedure. 
+      However, some medications can be resumed 4 hours after ingesting 
+      the capsule EXCEPT anticoagulants, Aspirin, NSAIDs.
+    </p>
+    <h2 className="sub-topic">The day of the procedure ?</h2>
+    <p>
+      Please come to the facility at your scheduled time. Wear loose 
+      fit clothing such as sweat pants. Once you are registered, 
+      and all the proper connections have been made, you are ready 
+      to swallow the pill. The capsule is the size of a vitamin pill 
+      and will be swallowed with a cup of water.
+    </p>
+    <h2 className="sub-topic">What is Expected after Ingesting the Capsule ?</h2>
+    <p>
+      You may drink clear liquids (water, apple juice) 2 hours after 
+      swallowing the pill. You may eat a light snack 4 hours after 
+      ingesting the pill, and 8 hours after, you may resume your 
+      regular diet. Avoid exercise and heavy lifting. You may walk, 
+      sit, lie down, and drive a car. If your job allows avoiding 
+      unsuitable environment or physical movements, then you can 
+      return to work. Avoid going near MRI machines and other equipment 
+      that have high frequency waves. DO NOT remove sensor array leads, 
+      and avoid getting them wet. Return to the facility the next day 
+      before 10:00am.
+      <br />
+      <br />
+      Please notify your physician if you experience severe or persistent 
+      abdominal pain, chest pain, fever, difficulty swallowing or if you 
+      have any questions.
+    </p>
+    <h2 className="sub-topic">Billing and Insurance</h2>
+    <p>
+      Capsule Endoscopy is a new procedure, some insurance companies 
+      may not cover the entire bill, therefore, patient may be 
+      responsible for part of the bill. Physician fees are separate 
+      from the facility's fee. Because patient will be carrying part 
+      of the instrument with them, there will be a $200.00 retaining 
+      fee. For your convenience, we accept cash, checks, VISA, 
+      MasterCard and Discover Card. Please note that the retaining fee 
+      will be reimbursed once the equipment is returned to the facility. 
+      If you have any questions regarding your bill, please feel free to 
+      call us.
+    </p>
   </div>
 )
 
