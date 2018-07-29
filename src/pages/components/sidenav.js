261cbc9cf92ecba6b6bf22dcffa2238f5395db5f
Shifu Yee <20980584+ShifuYee@users.noreply.github.com>
2018-07-28 23:02:51 -0400

    update: sidenav css

---
+++ src/pages/components/sidenav.js
@@ -0,0 +1,11 @@
+// React + Gatsby
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SideNav = () => (
+  <div className="sidenav">
+    <Link to="/resources/barrett/">GI Disorders</Link>
+  </div>
+)
+
+export default SideNav
