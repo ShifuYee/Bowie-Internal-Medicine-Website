99fe136cea2d4f80ddd85a98510e97b45e51ac3a
Shifu Yee <20980584+ShifuYee@users.noreply.github.com>
2018-07-31 18:04:04 -0400

    feat: added colon polyps content

--- src/components/sidenav-gd.js
+++ src/components/sidenav-gd.js
@@ -13,7 +13,7 @@ const SideNavGD = () => (
     <Link to="/resources/gi-disorders/helicobacterPylori/">Helicobacter Pylori</Link>
     <Link to="/resources/gi-disorders/gerd/">GastroEsophageal Reflux Disease (GERD)</Link>
     <h2>Lower GI Tract</h2>
-    <Link to="/resources/gi-disorders/barrett/">Colon Polyps & Cancer</Link>
+    <Link to="/resources/gi-disorders/colonPolyps/">Colon Polyps & Cancer</Link>
     <Link to="/resources/gi-disorders/barrett/">Diverticulosis & Diverticulitis</Link>
     <Link to="/resources/gi-disorders/barrett/">Crohn's Disease</Link>
     <Link to="/resources/gi-disorders/barrett/">Ulcerative Colitis</Link>
---
+++ src/pages/resources/gi-disorders/colonPolyps.js
@@ -0,0 +1,183 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+import SideNavGD from '../../../components/sidenav-gd'
+import homeimage from '../../../images/home-page-image.jpg'
+
+const ColonPolyps = () => (
+  <div className="service-content-resources">
+    <SideNavGD />
+    <div className="right-side">
+      <img alt="Home Page Image" src={homeimage} className="img-responsive" />
+      <h1 className="main-subject">Colon Polyps & Cancer</h1>		
+      <p>
+        Cancer of the colon is a major health problem in the United States. 
+        It ranks as a leading form of cancer, along with lung and breast cancer. 
+        Importantly, colon cancer is also one of the most curable forms of cancer. 
+        When detected early, more than 90 percent of patients can be cured.
+        This disease begins in the cells that line the colon. There is strong medical
+        evidence that abnormal genes that cause colon polyps and cancer can be passed 
+        from parent to child. 
+        <br/>
+        <br/>
+        The genes within each cell are t he hereditary structures that tell the 
+        cell what it should do. It is becoming increasingly clear that certain of 
+        these genes control the growth of the cells on the lining of the colon.
+        When these controlling genes are absent there is a tendency to grow polyps.
+        The cells in the polyp eventually become uncontrolled and turn into a cancer.
+        Colon cancer can also develop from other conditions, such as ulcerative 
+        colitis, a chronic inflammation in the colon.
+      </p>
+      <h2 className="sub-topic">What is a Colon Polyp ?</h2>
+      <p>
+        A polyp is a growth that occurs in the colon and other organs. These growths,
+        or fleshy tumors are shaped like a mushroom or a dome like button, and 
+        occur on the inside lining of the colon. They may be as small as a tiny 
+        pea or larger than a plum. It is important to note that while colon 
+        polyps start out as benign tumors, some polyps,in time, become malignant.
+        In fact, the larger the polyp, the more likely it is to contain cancer cells. 
+        In most cases, it is a polyp called an adenoma polyp that develops into colon cancer.
+      </p>
+      <h2 className="sub-topic">What are the Risk Factors for Developing Colon Polyps and Cancer ?</h2>
+      <ul>
+        <li>
+          Family history of polyps, cancer.
+        </li>
+        <li>
+          Western civilization and diet.
+        </li>
+        <li>
+          Being over 40 years of age.
+        </li>
+        <li>
+          History of breast cancer.
+        </li>
+        <li>
+          Ulcerative colitis.
+        </li>
+			</ul>
+      <p>
+        An individual whose parents, brothers or sisters have colon cancer is at 
+        increased risk. In addition, certain unusual conditions, such as
+        hereditary polyposis, lead to colon cancer 100 percent of the time.
+        Persons living and eating in Western societies are exposed to certain
+        factors that are detrimental to the bowel. Medicine is just beginning
+        to learn about these. Also, females who have had genital or breast 
+        cancer are at an increased risk of developing colon cancer. Ulcerative
+        colitis, after a long time, also can lead to colon malignancy.
+      </p>
+      <h2 className="sub-topic">Why Do Polyps Form ?</h2>
+      <p>
+        As noted above, heredity is probably the most important factor. 
+        The absence of a controlling gene within the cell is most likely
+        a necessary event. Other facts are known. Rural residents in 
+        Africa and India rarely develop colon polyps or cancer. Their 
+        diets are high in unprocessed grains (bran) and low in meat. Diets
+        that are high in bran and roughage produce bulky stools that have a 
+        lower bacteria count. 
+        <br/>
+        <br/>
+        Further, it is known that animal fat in meat, when broken down by 
+        the body's intestinal juices, may change into certain compounds 
+        termed carcinogens. These compounds cause colon cancer in 
+        laboratory animals. When residents in developing countries eat
+        a Westernized diet of meat, low roughage and refined grains, 
+        over time they develop the same incidence of colon polyps as people 
+        living in the Western world. So diet probably plays an important role 
+        in causing colon cancer.
+      </p>
+      <h2 className="sub-topic">Three Categories of Polyps</h2>
+      <p>
+        Enough is now known about polyps that physicians generally place
+				patients in one of three categories. In each of these the end
+				result is an adenoma type polyp:
+      </p>
+      <ol>
+        <li>
+          <u>The Ordinary Polyp</u> - Most sporatic polyps occur between the 
+          ages of 40 and 60. There may be only one or two present and 
+          they may take ten years or more to develop into a cancer. 
+          There is a hereditary link. 
+        </li>
+        <li>
+          <u>Familial Polyposis</u> - This is a true hereditary condition in which the
+          entire colon is studded with hundreds, even thousands of polyps.
+          They begin at a very early age even under ten years old. 
+          Virtually every patient will eventually develop colon cancer.
+          The only known preventive treatment is removal of the colon. 
+          Fortunately, the condition is not common. 
+        </li>
+        <li>
+          <u>Lynch Disorder</u> - This hereditary condition is more common than familial 
+          polyposis and less than the ordinary polyp. There is a strong tendency
+          for adenoma type polyps to develop in blood relatives; sisters, 
+          brothers, aunts, uncles and children. More polyps are seen and at
+          an early age. Polyps and even cancer can occur in the 30's and even 
+          20's. In some families there is an increased incidence of breast, 
+          ovarian, and other cancers. So a family history of this type warrants
+          very close surveillance of all direct blood relatives. 
+        </li>
+      </ol>
+      <h2 className="sub-topic">Detection</h2>
+      <p>
+        The key to early detection of colon polyps and cancer is the concern 
+        and willingness of each person to seek medical attention from a
+        physician. The physician will perform the following:
+      </p>
+      <ol>
+        <li>
+          <u>Medical history</u> - the patient's medical history will 
+          identify the presence of risk factors for colon polyps and cancer. 
+        </li>
+        <li>
+          <u>Stool exams to detect occult (hidden) blood</u> - colon cancers
+          and large polyps release minute quantities of blood; therefore, 
+          examination of the stool for occult blood is an important exam. 
+        </li>
+        <li>
+          <u>Sigmoidoscopy</u> - A visual exam of the rectum and lower colon, 
+          using a lighted, rigid or flexible tube, or endoscope, 
+          allows the direct detection of colon tumors. 
+        </li>
+        <li>
+          <u>Colonoscopy</u> - This procedure examines the entire 5-6 foot long colon.
+          Physicians perform it when any of the above three detection methods
+          suggest a problem. The exam is usually done under sedation. 
+          It is really the definitive test since colon polyps can be 
+          discovered and removed at the same time. 
+        </li>
+        <li>
+          <u>Barium Enema</u> - This x-ray exam is an older, at times still useful, exam.
+          Barium flows into the colon and x-rays are taken which then can
+          outline the shadows of polyps and cancer. Polyps cannot be 
+          removed by this technique. 
+        </li>
+        <li>
+          <u>Future tests</u> - It is possible that a blood genetic test may become 
+          available to identify those individuals who are destined to
+          develop colon polyps and cancer.
+        </li>
+      </ol>
+      <h2 className="sub-topic">How are Polyps removed ?</h2>
+      <p>
+        Removal of colon polyps reduces the risk of subsequent development
+				of cancer of the colon. Polyps are usually removed using a colonoscope,
+				which allows the physician to visualize the inside of the colon. Biopsy 
+				equipment and snares (wire loops) are passed through the endoscope and,
+				with electrical heat (eletrocautery), remove polyps and small tumors. 
+				This procedure is performed in the outpatient setting under sedation.
+      </p>
+      <h2 className="sub-topic">Summary</h2>
+      <p>
+        Cancer of the colon is a serious but readily detected malignancy. 
+				Early detection promises a particularly high chance of survival. 
+				Most colon cancers start as polyps, which can usually be removed 
+				through a colonoscopic exam. Today, there is much that can be done 
+				to prevent and cure this cancer. The essential first step involves 
+				action by the patient.
+      </p>
+    </div>
+  </div>
+)
+
+export default ColonPolyps
